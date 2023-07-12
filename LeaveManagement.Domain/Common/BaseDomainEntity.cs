eef161a12f9619429fc7b29ab560fcac915ee661
Foitelija <foitelija777@gmail.com>
2023-07-12 17:06:50 +0300

    feat: Setup Domain project

---
+++ LeaveManagement.Domain/Common/BaseDomainEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaveManagement.Domain.Common
+{
+    // абстрактный класс указывает на базовые поля, которые содержат все классы, которые наследуются от него
+    public abstract class BaseDomainEntity
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+        public string LastModifiedBy { get; set; }
+    }
+}
