fdddd365cb062dc31c9b216be116b508c84f98bd
Foitelija <foitelija777@gmail.com>
2023-07-13 13:52:33 +0300

    feat: Setup AutoMapper

---
+++ LeaveManagement.Application/ApplicationServicesRegistration.cs
@@ -0,0 +1,13 @@
+﻿using LeaveManagement.Application.Profiles;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LeaveManagement.Application
+{
+    public static class ApplicationServicesRegistration
+    {
+        public static void ConfigureApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(MappingProfile));
+        }
+    }
+}
