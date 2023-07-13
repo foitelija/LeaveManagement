c27e39b940586898f04fe5cd3ae68b1a8896a128
Foitelija <foitelija777@gmail.com>
2023-07-13 13:31:13 +0300

    feat: Setup Applicaiton Project using GenericRepository

---
+++ LeaveManagement.Application/Persistence/Contracts/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace LeaveManagement.Application.Persistence.Contracts
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IReadOnlyList<T>> GetAll();
+        Task<T> Get(int id);
+        Task<T> Add(T entity);
+        Task<T> Update(T entity);
+        Task<T> Delete(T entity);
+    }
+}
---
+++ LeaveManagement.Application/Persistence/Contracts/ILeaveRequestRepository.cs
@@ -0,0 +1,8 @@
+﻿using LeaveManagement.Domain;
+
+namespace LeaveManagement.Application.Persistence.Contracts
+{
+    public interface ILeaveRequestRepository : IGenericRepository<LeaveRequest>
+    {
+    }
+}
