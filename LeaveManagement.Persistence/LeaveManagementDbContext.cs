d2c6cad811cf232afda63e148856a370f7440975
Foitelija <foitelija777@gmail.com>
2023-07-16 14:40:06 +0300

    update: override SaveChangesAsync

--- LeaveManagement.Persistence/LeaveManagementDbContext.cs
+++ LeaveManagement.Persistence/LeaveManagementDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using LeaveManagement.Domain;
+using LeaveManagement.Domain.Common;
 using Microsoft.EntityFrameworkCore;
 
 namespace LeaveManagement.Persistence
@@ -13,6 +14,23 @@ namespace LeaveManagement.Persistence
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(LeaveManagementDbContext).Assembly);
+        }
+
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach(var entry in ChangeTracker.Entries<BaseDomainEntity>())
+            {
+                entry.Entity.LastModifiedDate = DateTime.Now;
+
+                if(entry.State == EntityState.Added)
+                {
+                    entry.Entity.DateCreated = DateTime.Now;
+                }
+            }
+
+
+            return base.SaveChangesAsync(cancellationToken);
         }
 
         public DbSet<LeaveRequest> LeaveRequests { get; set; }
